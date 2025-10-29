97ab4b1e9a17a21f887f2e1d12a6d331011bec89
zackyaundra-prog <zackyaundra@gmail.com>
2025-10-29 12:06:02 +0700

    farel

--- script.js
+++
@@ -1,6 +0,0 @@
-// Placeholder untuk interaktivitas tambahan, misal alert saat klik link (bisa dihapus jika tidak perlu)
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function() {
-        console.log('Navigating to: ' + this.href);
-    });
-});
